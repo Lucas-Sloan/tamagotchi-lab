d427c3c4e3b2855703122975ecb3887066197c95
Lucas-Sloan <Lucasg.sloan@gmail.com>
2024-07-17 16:11:09 -0500

    Added styling, centered game container, removed pseudocode, added baseline for instructions  in HTML

--- js/app.js
+++ js/app.js
@@ -1,17 +1,4 @@
 
-// 3) Upon loading, the game state should be initialized, and a function should 
-//    be called to render this game state.
-
-// 4) The state of the game should be rendered to the user.
-
-// 5) Handle the game over logic. 
-
-// 6) Handle each instance of a player clicking a button with the use of a 
-//    `handleClick()` function.
-
-// 7) Create reset functionality.
-
-
 /*-------------------------------- Constants --------------------------------*/
 
 const MAX_STAT = 10;
